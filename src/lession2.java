99c8bb8df122e4b5ec509652ad95c1bc61bb4e4d
dragoonvn <trietvm@hotmail.com>
2020-06-15 16:31:11 +0700

    activity-22

---
+++ src/lession2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class lession2 {
+    public static void main(String[] args) {
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số a: ");
+        a = scanner.nextInt();
+        int b = a + ((a * 10) + a) + ((a * 100) + (a * 10) + a);
+        System.out.printf(a + " + " + a + a + " + " + a + a + a + " = " + b);
+    }
+}
